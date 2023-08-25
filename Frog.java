36373a60592c95e589d33fcefaf4cef1219b4795
AlxanderSuire <142824203+AlxanderSuire@users.noreply.github.com>
2023-08-25 16:28:56 +0000

    overlaps added

--- Frog.java
+++ Frog.java
@@ -8,4 +8,8 @@ public class Frog{
   public Frog(Image i, DrawingCanvas c){
     frogImage = new VisibleImage(i, new Location(10, 10), c);
   }
+
+  public boolean overlaps(VisibleImage vehicleImage){
+    return false; //you need to change this
+  }
 }
