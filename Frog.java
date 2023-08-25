8766454be339e518cdb5877b14cd9c819eb4e3d9
KyleJG06 <142824170+KyleJG06@users.noreply.github.com>
2023-08-25 16:23:52 +0000

    added constructor

--- Frog.java
+++ Frog.java
@@ -3,4 +3,9 @@ import java.awt.*;
 public class Frog{
   private VisibleImage frogImage;
   private static final double FROG_HEIGHT = 48;
+
+
+  public Frog(Image i, DrawingCanvas c){
+    frogImage = new VisibleImage(i, new Location(10, 10), c);
+  }
 }
